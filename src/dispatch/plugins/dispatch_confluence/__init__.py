09aeb7587af71dc8cd6c0ad45fc7e4165a105a8d
Antoine Barthelemy <antoine@linux.com>
2022-06-16 12:08:59 +0200

    test(plugins): empty confluence load

--- src/dispatch/plugins/dispatch_confluence/__init__.py
+++ src/dispatch/plugins/dispatch_confluence/__init__.py
@@ -1,5 +1,1 @@
-try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution(__name__).version
-except Exception as e:
-    VERSION = '0.0.1'
+from ._version import __version__
--- src/dispatch/plugins/dispatch_confluence/plugin.py
+++ src/dispatch/plugins/dispatch_confluence/plugin.py
@@ -1,14 +1,18 @@
-import dispatch_pluginname
+import dispatch_confluence
 from dispatch.plugins.base.conversation import ConversationPlugin
+#from dispatch.plugins.dispatch_confluence import drive as google_drive_plugin
+
 
 class ConfluencePlugin(ConversationPlugin):
     title = 'Confluence'
     slug = 'confluence'
     description = 'Plugin generating confluence post-mortem page.'
     version = dispatch_pluginname.VERSION
+    version = google_drive_plugin.__version__
 
     author = 'Ant0wan'
-    author_url = 'https://github.com/ContentSquare/platform_houston/dispatch_plugin'
+    #author_url = 'https://github.com/ContentSquare/platform_houston/dispatch_plugin'
+    author_url = "https://github.com/netflix/dispatch.git"
 
     def create(self, items, **kwargs):
         return "Conversation Created"
@@ -18,3 +22,7 @@ class ConfluencePlugin(ConversationPlugin):
 
     def send(self, items, **kwargs):
         return "Message sent"
+
+    def __init__(self):
+       # self.configuration_schema = GoogleConfiguration
+        self.scopes = ["https://www.googleapis.com/auth/drive"]
