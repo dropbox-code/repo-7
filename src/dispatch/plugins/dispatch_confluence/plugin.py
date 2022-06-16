343a64915fcb8567c8e2b70f1e5a5d915701c1e7
Antoine Barthelemy <antoine@linux.com>
2022-06-16 16:31:22 +0200

    test(plugin): creating the doc plugin

--- src/dispatch/plugins/dispatch_confluence/plugin.py
+++ src/dispatch/plugins/dispatch_confluence/plugin.py
@@ -1,6 +1,9 @@
-from dispatch.plugins.bases import StoragePlugin
+from dispatch.plugins.bases import DocumentPlugin
 
-class ConfluencePlugin(StoragePlugin):
+__version__ = "0.1.0"
+
+
+class ConfluencePlugin(DocumentPlugin):
     title = 'Confluence Plugin - Document Management'
     slug = 'confluence-doc'
     description = 'Uses Confluence to help manage external documentation.'
@@ -8,15 +11,17 @@ class ConfluencePlugin(StoragePlugin):
     author = 'Ant0wan'
     author_url = "https://github.com/contentsquare/dispatch.git"
 
-    def create(self, items, **kwargs):
-        return "Conversation Created"
+    def get(self, key, **kwargs):
+        return
 
-    def add(self, items, **kwargs):
-        return "User Added"
+    def create(self, key, **kwargs):
+        return
 
-    def send(self, items, **kwargs):
-        return "Message sent"
+    def update(self, key, **kwargs):
+        return
 
-    def __init__(self):
-       # self.configuration_schema = GoogleConfiguration
-        self.scopes = ["https://www.googleapis.com/auth/drive"]
+    def delete(self, key, **kwargs):
+        return
+
+    def move(self, key, **kwargs):
+        return
