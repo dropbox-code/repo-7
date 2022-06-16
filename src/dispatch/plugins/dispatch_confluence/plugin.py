eb74a54f5ad9273b009e60f9648c6a3494114d35
Antoine Barthelemy <antoine@linux.com>
2022-06-16 16:49:14 +0200

    test(plugin)

--- src/dispatch/plugins/dispatch_confluence/plugin.py
+++ src/dispatch/plugins/dispatch_confluence/plugin.py
@@ -1,13 +1,18 @@
 from dispatch.plugins.bases import DocumentPlugin
+import dispatch.plugins.dispatch_dispacth as confluence_plugin
+
 
 __version__ = "0.1.0"
 
 
+#@apply(timer, exclude=["__init__"])
+#@apply(counter, exclude=["__init__"])
 class ConfluencePlugin(DocumentPlugin):
     title = 'Confluence Plugin - Document Management'
     slug = 'confluence-doc'
     description = 'Uses Confluence to help manage external documentation.'
-    version = "0.0.1"
+    version = confluence_plugin.__version__
+
     author = 'Ant0wan'
     author_url = "https://github.com/contentsquare/dispatch.git"
 
