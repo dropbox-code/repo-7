f12bd852b6bc23f5eab3e392f43086c74ab636c4
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-04-06 17:26:46 +0300

    Nikolaos Giagkinis completed the 2nd half of core exercises

--- src/for-loop-and-arrays.js
+++ src/for-loop-and-arrays.js
@@ -11,21 +11,27 @@ for (let i = 0; i < nums.length; i++) {
 }
 // 2. Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
-for (i = 0; i < nums.length ; i++) {
+for (let i = 0; i < nums.length; i++) {
   doubledNums.push(nums[i] * 2)
 }
 
 // 3. Use a for loop to set word equal to all the letters in the letters array
-for (i = 0; i < letters.length; i++) {
+for (let i = 0; i < letters.length; i++) {
   word += letters[i]
 }
-console.log(word);
 
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
+for (let i = 1; i < nums.length; i += 2) {
+  everySecondNum.push(nums[i])
+}
 
 // 5. Use a for loop to populate numsReversed with the numbers from nums in reverse order
 const numsReversed = []
+
+for (let i = nums.length - 1; i >= 0; i--) {
+  numsReversed.push(nums[i])
+}
 
 // do not change below this line
 module.exports = {
--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -12,8 +12,14 @@ for (let i = 5; i < 11; i++) {
   numsFiveToTen.push(i)
 }
 // TODO: 3. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
+for (let i = 0; i < 4; i++) {
+  evenNums.push(i * 2)
+}
 
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
+for (let i = 3; i >= 0; i--) {
+  countdown.push(i)
+}
 
 // do not change below this line
 module.exports = {
