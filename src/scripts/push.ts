3211e3ec9134fa756c2f53c3eb9a763303bd1b04
Luke Walton <thelukewalton@users.noreply.github.com>
2024-08-22 19:06:08 +0100

    fix: Don't commit coverage directory (#35)

--- src/scripts/push.ts
+++ src/scripts/push.ts
@@ -2,6 +2,7 @@ import { endGroup, setFailed, setOutput, startGroup } from "@actions/core";
 import { exec } from "@actions/exec";
 import { execSync } from "child_process";
 import { debug } from "@actions/core";
+import { start } from "repl";
 
 /**
  * Push changes to the branch
@@ -17,22 +18,42 @@ export const push = async (coverageDirectory: string) => {
     console.error("Unable to check if there are changes", e);
   }
   endGroup();
+  startGroup("Parse changes and remove coverage directory");
+  const changes = stdout.split("\n").filter((line) => line.trim() !== "");
+  if (changes.length === 1 && changes[0].includes(coverageDirectory)) {
+    changes.pop();
+  }
+  endGroup();
 
-  /// If `stdout` is empty, there are no changes
-  if (stdout != "") {
+  if (changes.length > 0) {
     try {
-      startGroup("Push changes");
+      startGroup("Set up git");
       await exec('git config --global user.name "github-actions"');
       await exec('git config --global user.email "github-actions@github.com"');
-      await exec(`git add -A -- ':!${coverageDirectory}'`);
+      endGroup();
+
+      try {
+        startGroup("Stage files");
+        await exec(`git add -A -- :!${coverageDirectory}/`);
+        await exec(`git add -A -- :!${coverageDirectory}/*`);
+      } catch (e) {
+        console.error("Unable to add files", e);
+      } finally {
+        endGroup();
+      }
+      startGroup("Commit changes");
       execSync(`git commit -m 'chore(automated): Lint commit and format' `);
+      endGroup();
+      startGroup("Push changes");
       await exec("git push -f");
+
       debug("Changes pushed onto branch");
+      endGroup();
     } catch (e) {
       console.error("Unable to push changes", e);
       setFailed("Unable to push changes to branch");
-    } finally {
-      endGroup();
     }
   }
 };
+
+push(".coverage");
